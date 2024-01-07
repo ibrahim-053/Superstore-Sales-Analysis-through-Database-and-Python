6ba3b5f3d231e5ac6ad4523e67d9f82ebe39e0e4
ibrahim-053 <149279318+ibrahim-053@users.noreply.github.com>
2024-01-08 00:06:29 +0100

    Add files via upload

---
+++ Database_Schema.sql
@@ -0,0 +1,38 @@
+CREATE TABLE Customers (
+    Customer_ID VARCHAR(20) PRIMARY KEY,
+    Customer_Name VARCHAR(100),
+    Segment VARCHAR(50)
+);
+
+CREATE TABLE Geography (
+    Customer_ID VARCHAR(20) REFERENCES Customers(Customer_ID),
+    Country VARCHAR(50),
+    City VARCHAR(50),
+    State VARCHAR(50),
+    Postal_Code INT,
+    Region VARCHAR(50),
+    PRIMARY KEY(Customer_ID)
+);
+
+CREATE TABLE Products (
+    Product_ID VARCHAR(20) PRIMARY KEY,
+    Category VARCHAR(50),
+    Sub_Category VARCHAR(50),
+    Product_Name VARCHAR(150)
+);
+
+CREATE TABLE Sales (
+    Product_ID VARCHAR(20) REFERENCES Products(Product_ID),
+    Sales DECIMAL(10, 2),
+    Quantity INT,
+    Discount DECIMAL(5, 2),
+    Profit DECIMAL(10, 2), 
+    PRIMARY KEY(Product_ID)
+);
+
+CREATE TABLE Orders (
+    Order_ID VARCHAR(20) PRIMARY KEY,
+    Order_Date DATE,
+    Ship_date DATE,
+    Ship_mode VARCHAR(50)
+);
